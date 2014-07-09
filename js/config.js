3e5584fc033ea7e679a11a8ab5c924e848f194eb
Andres Sebastian Garcia Scardilli <andres.garcia@startmeapps.com>
2014-07-09 03:01:29 -0300

    Adding pages

---
+++ js/config.js
@@ -0,0 +1,132 @@
+/* Google MAP Settings*/
+var lat= -34.6422262;  //Change the value with your address Latitude
+var lng= -58.5169238;  //Change the value with your address Longitude
+
+
+//Skill Chart Data
+var skillData = {
+    skill1: [
+        {
+            title: "C++ 80%",
+            color: "#f3377a",
+            border: "4px solid #f4709f",
+            value: '80%'
+        },
+        {
+            title: "Ruby 90%",
+            color: "#f3377a",
+            border: "4px solid #f4709f",
+            value: '90%'
+        },
+        {
+            title: "Java 50%",
+            color: "#f3377a",
+            border: "4px solid #f4709f",
+            value: '50%'
+        },
+        {
+            title: "PHP 60%",
+            color: "#f3377a",
+            border: "4px solid #f4709f",
+            value: '60%'
+        },
+        {
+            title: "SQL 80%",
+            color: "#f3377a",
+            border: "4px solid #f4709f",
+            value: '80%'
+        }
+    ],
+    skill2: [
+        {
+            title: "HTML5/CSS3 60%",
+            color: "#1b7ee4",
+            border: "4px solid #8cc2f9",
+            value: '60%'
+        },
+        {
+            title: "JavaScript 90%",
+            color: "#1b7ee4",
+            border: "4px solid #8cc2f9",
+            value: '90%'
+        },
+        {
+            title: "BootStrap 3 55%",
+            color: "#1b7ee4",
+            border: "4px solid #8cc2f9",
+            value: '55%'
+        },
+        {
+            title: "AngularJS 70%",
+            color: "#1b7ee4",
+            border: "4px solid #8cc2f9",
+            value: '70%'
+        },
+        {
+            title: "Web Design 50%",
+            color: "#1b7ee4",
+            border: "4px solid #8cc2f9",
+            value: '50%'
+        }
+    ],
+    skill3: [
+        {
+            title: "Testing 90%",
+            color: "#87b615",
+            border: "4px solid #e4f9b1",
+            value: '90%'
+        },
+        {
+            title: "OOP 100%",
+            color: "#87b615",
+            border: "4px solid #e4f9b1",
+            value: '100%'
+        }
+    ],
+    skill4: [
+        {
+            title: "Rails 95%",
+            color: "#CD950D",
+            border: "4px solid #FBE2A6",
+            value: '95%'
+        },
+        {
+            title: "Linux 80%",
+            color: "#CD950D",
+            border: "4px solid #FBE2A6",
+            value: '80%'
+        },
+        {
+            title: "MySql 70%",
+            color: "#CD950D",
+            border: "4px solid #FBE2A6",
+            value: '70%'
+        },
+        {
+            title: "API 80%",
+            color: "#CD950D",
+            border: "4px solid #FBE2A6",
+            value: '80%'
+        }
+    ],
+    skill5: [
+        {
+            title: "TDD 70%",
+            color: "#01A785",
+            border: "4px solid #8DEAD7",
+            value: '70%'
+        },
+        {
+            title: "Agiles 80%",
+            color: "#01A785",
+            border: "4px solid #8DEAD7",
+            value: '80%'
+        },
+        {
+            title: "Scrum 40%",
+            color: "#01A785",
+            border: "4px solid #8DEAD7",
+            value: '40%'
+        }
+    ]
+}
---
+++ js/script.js
@@ -0,0 +1,437 @@
+//Preloader
+$(window).load(function() {
+	$("#intro-loader").delay(1000).fadeOut();
+	$(".mask").delay(1000).fadeOut("slow");
+});
+
+$(document).ready(function() {
+
+  initializeMap(lat,lng);
+  
+	//Elements Appear from top
+	$('.item_top').each(function() {
+		$(this).appear(function() {
+			$(this).delay(200).animate({
+				opacity : 1,
+				top : "0px"
+			}, 1000);
+		});
+	});
+
+	//Elements Appear from bottom
+	$('.item_bottom').each(function() {
+		$(this).appear(function() {
+			$(this).delay(200).animate({
+				opacity : 1,
+				bottom : "0px"
+			}, 1000);
+		});
+	});
+	//Elements Appear from left
+	$('.item_left').each(function() {
+		$(this).appear(function() {
+			$(this).delay(200).animate({
+				opacity : 1,
+				left : "0px"
+			}, 1000);
+		});
+	});
+	
+	//Elements Appear from right
+	$('.item_right').each(function() {
+		$(this).appear(function() {
+			$(this).delay(200).animate({
+				opacity : 1,
+				right : "0px"
+			}, 1000);
+		});
+	});
+	
+	//Elements Appear in fadeIn effect
+	$('.item_fade_in').each(function() {
+		$(this).appear(function() {
+			$(this).delay(250).animate({
+				opacity : 1,
+				right : "0px"
+			}, 1500);
+		});
+	});
+
+	$("#nav").sticky({
+		topSpacing : 0
+	});
+
+  rotate('rotate1');
+
+    // Galleries Slider
+	$('.slider_container').flexslider({
+			directionNav: true,
+			controlNav: false
+		});
+	/*===================================================================================*/
+	/*  PROFOLIO                                                                         */
+	/*===================================================================================*/
+  	var portfolio = portfolio || {},
+		$portfolioItems       = $('#portfolio-items'),
+		$filtrable            = $('#portfolio-filter');
+
+		/*===================================================================================*/
+		/*  PROFOLIO INIT FULL WIDTH                                                         */
+		/*===================================================================================*/
+		portfolio.fullWidth = function(){
+
+	        function portfolioCol() {
+	         var winWidth = jQuery(window).width() + 15, columnNumb = 1;
+    if (winWidth > 1024) {
+      columnNumb = 4;
+    } else if (winWidth > 768) {
+      columnNumb = 3;
+    } else if (winWidth > 480) {
+      columnNumb = 2;
+    } else if (winWidth < 480) {
+      columnNumb = 1;
+    }
+    return columnNumb;
+	        }
+
+	        function setCol() {
+
+	            var width = $(window).width(),
+	                column = portfolioCol(),
+	                articleWidth =  Math.floor(width/column);
+
+	            $portfolioItems.find('article').each(function () {
+	                $(this).css( {
+	                    width : articleWidth + 'px'
+	                });
+	            });
+	        }
+
+
+	        $(window).load(function(){
+				setCol();
+	            $portfolioItems.isotope({
+					animationEngine: 'best-available',
+					animationOptions: {
+							duration: 250,
+							easing: 'easeInOutSine',
+							queue: false
+				   }
+				});
+	        });
+
+			$(window).bind('resize', function () {
+				setCol();
+				$portfolioItems.isotope('reLayout');
+			});
+
+	        $filtrable.find('a').click(function(e) {
+	            var currentOption = $(this).data('cat');
+
+	            $filtrable.find('a').removeClass('active');
+	            $(this).addClass('active');
+
+	            if (currentOption !== '*') {
+	            	currentOption = '.' + currentOption;
+	            }
+
+	           	$portfolioItems.isotope({filter : currentOption});
+	            return false;
+	        });
+
+		};
+
+		/*===================================================================================*/
+		/*  PROFOLIO INIT AJAX                                                               */
+		/*===================================================================================*/
+
+
+		portfolio.ajax = function(){
+
+
+			function portfolioInit() {
+
+                var newHash      = "",
+                    $mainContent = $("#portfolio-ajax"),
+                    $pageWrap    = $("#portfolio-wrap"),
+				          	root         = '#!projects/',
+                    rootLength   = root.length,
+                    url;
+
+                $portfolioItems.find("a").click(function() {
+                    window.location.hash = $(this).attr("href");
+                    return false;
+                });
+
+                $(window).bind('hashchange', function(){
+
+					newHash = window.location.hash;
+				    url = newHash.replace(/[#\!]/g, '' );
+                    if (newHash.substr(0,rootLength) == root) {
+                    	if($pageWrap.is(':hidden')){
+                    		$pageWrap.slideDown('3000', function(){});
+                    	}
+						$pageWrap.niceScroll({
+				            cursorcolor:"#666",
+				            cursorwidth:6,
+				            cursorborder: 0,
+				            cursorborderradius: 0
+				        });
+                        $pageWrap.append('<div id="preloader"></div>');
+                        $mainContent.load(url + " .single-portfolio", function(xhr, statusText, request){
+                        	if(statusText == "success"){
+									             setTimeout(function () {
+                                $(".slider_container").flexslider({directionNav: true,controlNav: false	}); 
+                                $('.single-portfolio .media-container').fitVids();
+		                        		$pageWrap.find('#preloader').remove();
+		                            }, 300);
+                            }    
+		                    
+							if(statusText == "error"){
+								$mainContent.html('<div class="row pad-top pad-bottom"><div class="col-md-12 pad-top pad-bottom"><div class="alert-message error"><p>The Content cannot be loaded.</p></div></div></div>');
+		                        $pageWrap.find('#preloader').remove();
+							}
+	                        closeProject();
+	                        nextProject();
+	                        prevProject();
+                       });
+
+	                   $("#portfolio-items article").removeClass("current");
+	                   $("#portfolio-items a[href='" + newHash + "']").parent().addClass("current");
+                     var projectIndex = $('#portfolio-items').find('article.current').index();
+						var projectLength = $('#portfolio-items article').length -1;
+            
+						if(projectIndex == projectLength){
+
+							jQuery('#next-project').addClass('disabled');
+							jQuery('#prev-project').removeClass('disabled');
+
+						}else if(projectIndex == 0){
+
+							jQuery('#prev-project').addClass('disabled');
+							jQuery('#next-project').removeClass('disabled');
+
+						}else{
+							jQuery('#prev-project, #next-project').removeClass('disabled');
+
+						}
+                    }
+                    else if(newHash == '')
+                    {
+                    	$('#portfolio-wrap').fadeOut('100', function() {
+                        	$('.single-portfolio').remove();
+                    	});
+                    }
+                });
+
+                $(window).trigger('hashchange');
+            }
+
+
+
+            function closeProject() {
+				     $('#close-project').on('click', function() {
+                    $('#portfolio-wrap').fadeOut('100', function() {
+                        $('.single-portfolio').remove();
+                    });                  
+                    history.pushState('', document.title, window.location.pathname);
+                    window.location.hash = '#_';
+	                return false;
+	            });
+            }
+
+            function nextProject() {
+                $("#next-project").on("click", function() {
+                    $('.single-portfolio').remove();
+                    window.location.hash = $("#portfolio-items .current").next().find('a').attr("href");
+                    return false;
+                });
+            }
+
+
+            function prevProject() {
+                $("#prev-project").on("click", function() {
+                    $('.single-portfolio').remove();
+                    window.location.hash = $("#portfolio-items .current").prev().find('a').attr("href");
+                    return false;
+                });
+            }
+
+            if ($portfolioItems.length) {
+                portfolioInit();
+            }
+		};
+		portfolio.fullWidth();
+		portfolio.ajax();
+		
+
+	// Contact Form Request
+  $(".validate").validate();	
+  var form = $('#contactform');
+	var submit = $('#contactForm_submit');	
+	var alertx = $('.form-respond'); 
+
+  	// form submit event
+    $(document).on('submit', '#contactform', function(e) {
+		e.preventDefault(); // prevent default form submit
+		// sending ajax request through jQuery
+		$.ajax({
+			url: 'https://fwdform.herokuapp.com/user/18bbbed4-645b-41be-ac43-cd12626f19f9',
+			type: 'POST',
+            crossDomain: true,
+			dataType: 'html',
+			data: form.serialize(), 
+			beforeSend: function() {
+				alertx.fadeOut();
+				submit.val('Sending....'); // change submit button text
+			},
+			success: function(data) {
+				form.fadeOut(300);
+        alertx.html(data).fadeIn(1000); // fade in response data     
+            setTimeout(function() {
+          alertx.html(data).fadeOut(300);
+          $('#name, #email, #message').val('')
+          form.fadeIn(1800);
+       }, 4000 );  
+
+			},
+			error: function(e) {
+                var data= "El mensaje se ha enviado"
+                form.fadeOut(300);
+                alertx.html(data).fadeIn(1000); // fade in response data
+                setTimeout(function() {
+                    alertx.html(data).fadeOut(300);
+                    submit.val('Enviar'); // change submit button text
+                    $('#name, #email, #message').val('')
+                    form.fadeIn(1800);
+                }, 4000 );
+
+            }
+		});
+	});
+
+
+	//Navigation Dropdown
+	$('.nav a.collapse-menu').click(function() {
+		$(".navbar-collapse").collapse("hide")
+	});
+
+	$('body').on('touchstart.dropdown', '.dropdown-menu', function(e) {
+		e.stopPropagation();
+	});
+
+ 
+	//Back To Top
+	$(window).scroll(function() {
+		if ($(window).scrollTop() > 400) {
+			$("#back-top").fadeIn(200);
+		} else {
+			$("#back-top").fadeOut(200);
+		}
+	});
+	$('#back-top').click(function() {
+		$('html, body').stop().animate({
+			scrollTop : 0
+		}, 1500, 'easeInOutExpo');
+	});
+
+});
+
+//Navigation Scrolling
+	$(function() {
+		$('.nav li a').bind('click', function(event) {
+			var $anchor = $(this);
+
+			$('html, body').stop().animate({
+				scrollTop : $($anchor.attr('href')).offset().top - 70
+			}, 2000, 'easeInOutExpo');
+
+			event.preventDefault();
+		});
+	});
+   
+//FullScreen Slider
+$(function(){
+$('#fullscreen-slider').maximage({
+cycleOptions: {
+fx: 'fade',
+speed: 1000,
+timeout: 5000,
+pause: 1
+},
+onFirstImageLoaded: function(){
+jQuery('#cycle-loader').hide();
+jQuery('#fullscreen-slider').fadeIn('slow');
+},
+// cssBackgroundSize might be causing choppiness in retina display safari
+cssBackgroundSize: false
+});
+});
+
+//Parallax
+$(window).bind('load', function() {
+    if(!onMobile)
+	parallaxInit();
+});
+
+function parallaxInit() {
+	$('#one-parallax').parallax("50%", 0.2);
+	$('#two-parallax').parallax("50%", 0.2);
+	$('#three-parallax').parallax("50%", 0.2);
+	/*add as necessary*/
+}
+	var onMobile = false;
+	if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
+		onMobile = true;
+	}
+
+/*-----------------------------------
+Counter
+-----------------------------------*/
+
+$(function() {
+  "use strict";
+    $(".number-counters").appear(function(){
+        $(".number-counters [data-to]").each(function(){
+            var count = $(this).attr('data-to');
+            $(this).delay(6000).countTo({
+                from: 50,
+                to: count,
+                speed: 3000,
+                refreshInterval: 50,
+            });
+        });
+        $(".number-counters [data-to-percentage]").each(function(){
+            var count = $(this).attr('data-to-percentage');
+            $(this).delay(6000).countTo({
+                from: 0,
+                to: count,
+                formatter: function(value, options){
+                    return "%"+value.toFixed(options.decimal)
+                },
+                speed: 3000,
+                refreshInterval: 50,
+            });
+        });
+    });
+});
+
+//Initilize Google Map
+ function initializeMap(lat,lng) {
+     var mapOptions = {
+       center: new google.maps.LatLng(lat, lng),
+       zoom: 16,
+       zoomControl: true,
+       scaleControl: false,
+       scrollwheel: false,
+       mapTypeId: google.maps.MapTypeId.ROADMAP
+     };
+     var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+     var marker = new google.maps.Marker({
+     position: mapOptions['center'],
+     map: map,
+     });
+       
+     return map;
+ }
+
